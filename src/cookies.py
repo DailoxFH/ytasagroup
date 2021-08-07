e65be01f0b1375e27c397b1ac6cf86f452bd7fe0
DailoxFH <dail0x@protonmail.com>
2021-08-07 08:24:29 +0200

    Added samesite and secure attribute for cookie

--- src/cookies.py
+++ src/cookies.py
@@ -2,11 +2,12 @@ from flask import make_response, redirect, url_for
 import hashlib
 from src.generator import generate_random, remove_risky
 
+
 def create_cookie(room_id, username):
     resp = make_response(redirect(url_for("watch_yt", roomid=room_id), code=307))
     id = generate_random(24, True)
     username = remove_risky(username)
-    resp.set_cookie(username, id)
+    resp.set_cookie(username, id, samesite="strict", secure=True)
     h = hashlib.sha512(id.encode())
     return [resp, username, h.hexdigest()]
 
